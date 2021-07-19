8f67ff8ac6052fbacff68e69d0831061406e944b
daizhichao <daizhichao0822@gmail.com>
2021-07-19 15:56:38 +0800

    feat: 新增 tailwind

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import router from './route';
 import ElementPlus from 'element-plus';
 import 'element-plus/lib/theme-chalk/index.css';
 import "tailwindcss/tailwind.css"
+import "./design/tailwind.css"
+
 console.log(process.env.NODE_ENV)
 
 createApp(App).use(router).use(ElementPlus).mount('#app')
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,41 +1,61 @@
 <template>
-  <div class="flex flex-row">
-    <div>1</div>
-    <div>2</div>
-    <div>3</div>
+<div class="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+  <div class="relative py-3 sm:max-w-xl sm:mx-auto">
+    <div class="absolute inset-0 bg-gradient-to-r from-cyan-400 to-sky-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
+    <div class="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+      <div class="max-w-md mx-auto">
+        <div>
+          <img src="@/assets/logo.png" class="h-7 sm:h-8" />
+        </div>
+        <div class="divide-y divide-gray-200">
+          <div class="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
+            <p>An advanced online playground for Tailwind CSS, including support for things like:</p>
+            <ul class="list-disc space-y-2">
+              <li class="flex items-start">
+                <span class="h-6 flex items-center sm:h-7">
+                  <svg class="flex-shrink-0 h-5 w-5 text-cyan-500" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+                  </svg>
+                </span>
+                <p class="ml-2">
+                  Customizing your
+                  <code class="text-sm font-bold text-gray-900">tailwind.config.js</code> file
+                </p>
+              </li>
+              <li class="flex items-start">
+                <span class="h-6 flex items-center sm:h-7">
+                  <svg class="flex-shrink-0 h-5 w-5 text-cyan-500" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+                  </svg>
+                </span>
+                <p class="ml-2">
+                  Extracting classes with
+                  <code class="text-sm font-bold text-gray-900">@apply</code>
+                </p>
+              </li>
+              <li class="flex items-start">
+                <span class="h-6 flex items-center sm:h-7">
+                  <svg class="flex-shrink-0 h-5 w-5 text-cyan-500" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+                  </svg>
+                </span>
+                <p class="ml-2">Code completion with instant preview</p>
+              </li>
+            </ul>
+            <p>Perfect for learning how the framework works, prototyping a new idea, or creating a demo to share online.</p>
+          </div>
+          <div class="pt-6 text-base leading-6 font-bold sm:text-lg sm:leading-7">
+            <p>Want to dig deeper into Tailwind?</p>
+            <p>
+              <a href="https://tailwindcss.com/docs" class="text-cyan-600 hover:text-cyan-700"> Read the docs &rarr; </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
-  <ul class="space-y-4">
-    <li>
-      <div class="w-64 h-3 bg-gradient-to-br from-fuchsia-500 to-purple-600"></div>
-    </li>
-    <li>
-      <div class="w-56 h-3 bg-gradient-to-br from-fuchsia-500 to-purple-600"></div>
-    </li>
-    <li>
-      <div class="w-48 h-3 bg-gradient-to-br from-fuchsia-500 to-purple-600"></div>
-    </li>
-    <li>
-      <div class="w-40 h-3 bg-gradient-to-br from-fuchsia-500 to-purple-600"></div>
-    </li>
-    <li>
-      <div class="w-32 h-3 bg-gradient-to-br from-fuchsia-500 to-purple-600"></div>
-    </li>
-    <li>
-      <div class="w-24 h-3 bg-gradient-to-br from-fuchsia-500 to-purple-600"></div>
-    </li>
-    <li>
-      <div class="w-20 h-3 bg-gradient-to-br from-fuchsia-500 to-purple-600"></div>
-    </li>
-    <li>
-      <div class="w-16 h-3 bg-gradient-to-br from-fuchsia-500 to-purple-600"></div>
-    </li>
-    <li>
-      <div class="w-12 h-3 bg-gradient-to-br from-fuchsia-500 to-purple-600"></div>
-    </li>
-    <li>
-      <div class="w-10 h-3 bg-gradient-to-br from-fuchsia-500 to-purple-600"></div>
-    </li>
-  </ul>
+</div>
+
 </template>
 
 <script lang="ts">
